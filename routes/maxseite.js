56247b2eaf227c29c58c6c9c58091b38b1c1a195
Snotters <46791650+Snotters@users.noreply.github.com>
2022-03-27 00:58:57 +0100

    Added DB data
    Added DB data

--- routes/maxseite.js
+++ routes/maxseite.js
@@ -8,6 +8,12 @@ router.get('/' , (req, res) => {
     res.send("Hey ich bin auf meiner Seite hier kommen die Ekko Stats hin");
 });
 
+
+router.post('/' , (req, res) => {
+    //Show Database data
+    console.log(req.body);
+});
+
 module.exports = router;
 
 
